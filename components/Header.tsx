b1f0813397ce7359457d435b0fd4b7bd163e30e7
Bee-code-tech <babawaleolatunji64@gmail.com>
2024-01-28 04:21:08 +0100

    progress so far

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,45 @@
+'use client'
+import { useState, useEffect } from "react";
+import  Logo  from "./Logo";
+import  Nav  from "./Nav";
+import  MobileNav  from "./MobileNav";
+
 import ThemeToggler from './ThemeToggler'
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const [header, setHeader] = useState<Boolean>(false)
+  const pathname = usePathname()
+  useEffect(() => {
+   const handleScroll = () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Call it immediately to set the initial state based on the current scroll position
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+    
+  }, [])
+  
   return (
-    <header>
+    <header className={` ${header ? 'py-4 bg-white shadow-lg dark:bg-accent ' : 'py-6 dark:bg-transparent'} sticky top-0 z-30 transition-all`}>
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div className="flex items-center gap-x-6 ">
+          <Nav containerStyles="hidden xl:flex gap-x-8 items-center" linkStyle="relative hover:text-primary transition-all" underLineStyles="absolute left-0 top-full h-[2px] bg-primary w-full" />
         <ThemeToggler />
+        {/* Mobile nav  */}
+        <div className=" xl: hidden">
+          <MobileNav />
+        </div>
+
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
---
+++ components/Nav.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import React, {FC} from 'react'
+import { usePathname } from "next/navigation"
+import { motion } from "framer-motion";
+
+
+interface NavProps {
+    containerStyles : string,
+    linkStyle: string,
+    underLineStyles : string
+}
+
+
+interface NavLink {
+  path: string;
+  name: string;
+}
+
+const links: NavLink[] = [
+  { path: '/', name: 'Home' },
+  { path: '/projects', name: 'My projects' },
+  { path: '/contact', name: 'Contact Me' },
+];
+
+const Nav : FC<NavProps> = ({containerStyles, linkStyle, underLineStyles}) => {
+    const paths = usePathname()
+  return (
+    <nav className={`${containerStyles}`}>
+      {
+        links.map(({path, name  }, index) => {
+            return <Link key={index} href={path} className={`${linkStyle}`} >
+                {path === paths && (
+                    <motion.span initial={{y: '-100px'}} animate ={{y: 0}} transition={{type: 'tween'}} layoutId='underline' className={`${underLineStyles}`} />
+                )}
+                {name}
+                </Link>
+        })
+      }
+    </nav>
+  )
+}
+
+export default Nav
